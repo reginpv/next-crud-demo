17d9d25318b472e01679c4420e9048907b4188f3
Gin <reginpv@gmail.com>
2024-12-10 01:29:46 +0800

    unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import prisma from "@/lib/prisma"
 import Link from "next/link"
 import FormDelete from "@/components/FormDelete"
-import FormFind from "@/components/FormFind"
 
 export default async function Home() {
 
